b0b25a3e31ea8ebf8645a6a2df377bf7b5aae1a3
Bryan Hundven <bryanhundven@gmail.com>
2012-08-19 14:17:49 -0700

    also exclude rsync'ing .gitmodules
    Signed-off-by: Bryan Hundven <bryanhundven@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull
 git submodule init
 git submodule update
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".gitmodules" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
